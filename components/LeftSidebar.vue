efb50164443b8c49650adb353da0668c7d6a192c
Santtu Söderholm <santtu.soderholm@protonmail.com>
2021-06-04 12:27:01 +0300

    Added a LeftSidebar component

---
+++ components/LeftSidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="sidebar">
+    <div class="icon--title">
+      <img
+        class="mr-1"
+        src="~/assets/icons/credentify.svg"
+        alt="Credentify logo"
+      />
+      Credentify
+    </div>
+    <div v-if="this.$auth.loggedIn" class="links">
+      <ul>
+        <li
+          v-for="menuitem in menu_items"
+          :key="menuitem.label"
+          class="sidebaritem"
+        >
+          <NuxtLink :to="menuitem.url">
+            <img :src="menuitem.icon" />
+            {{ menuitem.label }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    menu_items() {
+      return this.$store.state.nav.menuItems
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.sidebaritem {
+  align-items: left;
+  list-style-type: none;
+  width: 100%;
+
+  img {
+    margin-right: 10px;
+  }
+}
+</style>
